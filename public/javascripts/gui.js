c572600b9f78fa8eb6b4ede3354701d9af88bc98
tmwnd <timwende040501@outlook.de>
2022-06-06 00:00:25 +0200

    added waifu popup

--- public/javascripts/gui.js
+++ public/javascripts/gui.js
@@ -1,10 +1,10 @@
+const HOLD_TIME = 250;
+const POPUP_TPL = fetch("/templates/partials/waifu_popup.html").then(file => file.text())
+
 function toggle_check(event) {
     let class_list = event.target.classList
 
-    if (!class_list.contains('checked'))
-        class_list.add('checked')
-    else
-        class_list.remove('checked')
+    class_list.toggle('checked')
 
     if (class_list.contains('series')) {
         Array.from(event
@@ -48,7 +48,37 @@ function toggle_check(event) {
     }
 }
 
+async function waifu_popup(event) {
+    let tpl = await POPUP_TPL
+
+    let popup = document.getElementById('waifu_popup')
+    let waifu = event
+        .target
+        .textContent
+    let series = event
+        .target
+        .parentElement
+        .parentElement
+        .parentElement
+        .getElementsByClassName('series')[0]
+        .textContent
+    popup.innerHTML = Mustache.render(tpl, {
+        'img_url': 'https://bot.to/wp-content/uploads/2020/09/bongo_5f70261889b78.png',
+        'name': waifu,
+        'series': series
+    })
+
+    if (!popup.classList.contains('show'))
+        popup.classList.add('show')
+
+    document.getElementById('btn_popup').addEventListener('click', event => {
+        if (popup.classList.contains('show'))
+            popup.classList.remove('show')
+    })
+}
+
 function set_series_listener() {
+    let timeout_id = 0
     Array.from(document.getElementsByClassName('series')).forEach(series => {
         series.addEventListener('click', toggle_check)
         Array.from(series
@@ -56,7 +86,19 @@ function set_series_listener() {
             .getElementsByClassName('waifus_outer')[0]
             .getElementsByClassName('waifus_inner')[0]
             .children
-        ).forEach(waifu => waifu.addEventListener('click', toggle_check))
+        ).forEach(waifu => {
+            // waifu.addEventListener('click', toggle_check)
+            waifu.addEventListener('mousedown', event => timeout_id = setTimeout(() => {
+                timeout_id = 0
+                waifu_popup(event)
+            }, HOLD_TIME))
+            waifu.addEventListener('mouseup', event => {
+                if (timeout_id != 0) {
+                    clearTimeout(timeout_id)
+                    toggle_check(event)
+                }
+            })
+        })
     })
 }
 
